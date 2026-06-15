2cc17357458cd41d8824827bb779d85b2c19390e
Alex Han <alexhzc100318@gmail.com>
2026-06-15 14:21:20 -0600

    Add comment for list comprehension task
    Added a comment explaining the task of creating a new list with even elements from the original list.

--- #07_List_Comprehensions.py
+++ #07_List_Comprehensions.py
@@ -1,3 +1,6 @@
+#Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python 
+#that takes this list a and makes a new list that has only the even elements of this list in it
+
 #State: There is no state
 #Input: List saved in a variables
 #Rule: New list can only contain even elements from the original list 
