75209fd72d0b4f348f2b932007c9e4273cfcbdba
Alex Han <alexhzc100318@gmail.com>
2026-06-15 14:21:55 -0600

    Update #08_Rock_Paper_Scissors.py

--- #08_Rock_Paper_Scissors.py
+++ #08_Rock_Paper_Scissors.py
@@ -1,3 +1,6 @@
+#Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, 
+#and ask if the players want to start a new game)
+
 #State: condition of what users input
 #Input: Users type in conditions
 #Rule: #1: Rock > Scissors
