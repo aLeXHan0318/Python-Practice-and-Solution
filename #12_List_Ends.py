c235dac7987b831e7a279df794341665e9413542
Alex Han <alexhzc100318@gmail.com>
2026-06-16 22:30:09 -0600

    Update #12_List_Ends.py

--- #12_List_Ends.py
+++ #12_List_Ends.py
@@ -1,3 +1,8 @@
+"""
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list.
+For practice, write this code inside a function.
+"""
+
 #State:None
 #Input:None
 #Rule: List only contains the first and last elements from the given list
